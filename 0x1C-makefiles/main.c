6d3e9bab2f621e48b22c6aed3bea153d45d8dd49
suadcode <suadsalam7@gmail.com>
2024-01-27 05:55:13 -0800

    0-Makefile

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "message.h"
+
+/**
+ * main - Te starting point
+ * Return: Success Always
+ */
+
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+/* School */
---
+++ 0x1C-makefiles/message.h
@@ -0,0 +1,9 @@
+#ifndef __MESSAGE_H__
+#define __MESSAGE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_school(void);
+
+#endif
